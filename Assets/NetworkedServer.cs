e1f008659bdc641a13e8b8d5570f1d44c9695e4d
Luffy <luffyklose@gmail.com>
2021-10-10 16:06:31 -0400

    Implement login and create account
    Implement receive data from client and save;
    Implement receive data from client and verify;

--- Assets/NetworkedServer.cs
+++ Assets/NetworkedServer.cs
@@ -13,6 +13,7 @@ public class NetworkedServer : MonoBehaviour
     int unreliableChannelID;
     int hostID;
     int socketPort = 5491;
+    private LinkedList<PlayerAccount> playerAccounts;
 
     // Start is called before the first frame update
     void Start()
@@ -23,13 +24,14 @@ public class NetworkedServer : MonoBehaviour
         unreliableChannelID = config.AddChannel(QosType.Unreliable);
         HostTopology topology = new HostTopology(config, maxConnections);
         hostID = NetworkTransport.AddHost(topology, socketPort, null);
+        playerAccounts = new LinkedList<PlayerAccount>();
         
+        //Load player accounts list 
     }
 
     // Update is called once per frame
     void Update()
     {
-
         int recHostID;
         int recConnectionID;
         int recChannelID;
@@ -55,7 +57,6 @@ public class NetworkedServer : MonoBehaviour
                 Debug.Log("Disconnection, " + recConnectionID);
                 break;
         }
-
     }
   
     public void SendMessageToClient(string msg, int id)
@@ -68,6 +69,96 @@ public class NetworkedServer : MonoBehaviour
     private void ProcessRecievedMsg(string msg, int id)
     {
         Debug.Log("msg recieved = " + msg + ".  connection id = " + id);
+
+        string[] csv = msg.Split(',');
+
+        int signifier = int.Parse(csv[0]);
+
+        if (signifier == ClientToServerSignifiers.CreateAccount)
+        {
+            string n = csv[1];
+            string p = csv[2];
+
+            bool hasUser = false;
+            
+            foreach (var pa in playerAccounts)
+            {
+                if (pa.name == n)
+                {
+                   hasUser = true ;
+                    break;
+                }
+            }
+            if (!hasUser)
+            {
+                playerAccounts.AddLast(new PlayerAccount(n, p));
+                SendMessageToClient(ServerToClientSiginifiers.LoginResponse + "," + LoginResponses.Success, id);
+                
+                //save player account list
+            }
+            else
+            {
+                SendMessageToClient(ServerToClientSiginifiers.LoginResponse + "," + LoginResponses.FailureNameNotFound, id);
+            }
+        }
+        else if (signifier == ClientToServerSignifiers.Login)
+        {
+            string n = csv[1];
+            string p = csv[2];
+
+            bool hasBeenFound = false;
+            
+            foreach (var pa in playerAccounts)
+            {
+                if (pa.name == n)
+                {
+                    if (pa.password == p)
+                    {
+                        SendMessageToClient(ServerToClientSiginifiers.LoginResponse + "," + LoginResponses.Success, id);
+                    }
+                    else
+                    {
+                        SendMessageToClient(ServerToClientSiginifiers.LoginResponse + "," + LoginResponses.FailureIncorrectPassword, id);
+                    }
+                    hasBeenFound = true;
+                    break;
+                }
+            }
+            
+            if (!hasBeenFound)
+            {
+                SendMessageToClient(ServerToClientSiginifiers.LoginResponse + "," + LoginResponses.FailureNameInUse, id);
+            }
+        }
     }
 
+    public class PlayerAccount
+    {
+        public string name,password;
+        
+        public PlayerAccount(string name, string password)
+        {
+            this.name = name;
+            this.password = password;
+        }
+    }
+
+    public static class ClientToServerSignifiers
+    {
+        public const int Login = 1;
+        public const int CreateAccount = 2;
+    }
+
+    public static class ServerToClientSiginifiers
+    {
+        public const int LoginResponse = 1;
+    }
+
+    public static class LoginResponses
+    {
+        public const int Success = 1;
+        public const int FailureNameInUse = 2;
+        public const int FailureNameNotFound = 3;
+        public const int FailureIncorrectPassword = 4; 
+    }
 }
